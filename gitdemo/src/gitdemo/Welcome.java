100cbc9879547655c6611e0653e945718b944e38
GopiChalluri <gopi.challuri@cognizant.com>
2025-09-04 12:32:46 +0530

    welcom commit3

--- gitdemo/src/gitdemo/Welcome.java
+++ gitdemo/src/gitdemo/Welcome.java
@@ -8,6 +8,8 @@ System.out.println("Welcome to Git");
 System.out.println("Pushing the code to github");
 System.out.println("Pulling the code to github");
 System.out.println("Puhing  the code to Remote repo : attempt2");
+
+System.out.println("pushing to barnch 1");
 	}
 
 }
