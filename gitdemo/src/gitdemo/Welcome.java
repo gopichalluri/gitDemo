dfe1d969c8ed97bfcb7375bc93dd363efd9154ab
GopiChalluri <gopi.challuri@cognizant.com>
2025-09-04 11:53:31 +0530

    gitDemo commit

---
+++ gitdemo/src/gitdemo/Welcome.java
@@ -0,0 +1,12 @@
+package gitdemo;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+
+System.out.println("Welcome to Git");
+System.out.println("Pushing the code to github");
+
+	}
+
+}
